c7102c8dc00f098001ac53bcdcb56849126fb5b2
mmichaud-test <marmichaud@gmail.com>
2025-05-31 10:23:05 -0400

    menu_presque

--- script.js
+++ script.js
@@ -13,3 +13,23 @@ document.addEventListener("DOMContentLoaded", function () {
       console.error('Erreur lors du chargement du menu :', error);
     });
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+  const ulSym = document.getElementById("menuSym");
+  const ulAnc = document.getElementById("menuAnc");
+  const ulMon = document.getElementById("menuMon");
+  const currentUrl = window.location.pathname;
+
+  switch (currentPath) {
+    case "/contact":
+    case "/login":
+    case "/index_ancestries":
+      ulAnc.style.display = "block";
+      break;
+    
+    
+    default:
+      // Le menu reste visible
+      break;
+  }
+});
